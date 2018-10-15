9c324808a37292a1d7c0f9f71236c88a7e0983a7
gheinemann <guillaume.heinemann@vidal.fr>
2018-10-15 14:19:52 +0200

    Add component page

---
+++ src/Components_1_App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <span>cool</span>
+    <counter></counter>
+</div>
+</template>
+
+<script>
+import Counter from "./Counter";
+
+export default {
+  name: "App",
+  components: { Counter }
+};
+</script>
